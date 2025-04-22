29a04e2340b8bd682e5586ad159d7837f57c3a95
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-21 22:49:05 -0700

    fix(haskell): run.sh syntax fix

--- Haskell/Lexer/run.sh
+++ Haskell/Lexer/run.sh
@@ -5,7 +5,7 @@ binDir=$buildDir/bin
 
 mkdir -p $binDir
 
-run () { $binDir/main.out }
+run () { $binDir/main.out; }
 compile () { ghc -dynamic -o $binDir/main.out -odir $buildDir -hidir $buildDir Main.hs && run; }
 
 [[ -f $buildDir/Main.hi && -f $buildDir/Main.o ]] && run || compile
