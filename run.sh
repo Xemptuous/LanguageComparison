f54528ba74ef037fb78f33f07eadf5496070c609
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-21 22:49:28 -0700

    feat: global run.sh w/ optional compile flag

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+file=run.sh
+while getopts ":hcr" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 [-h] [-c r] language project"
+      exit 0
+      ;;
+    c)
+      file=compile.sh
+      ;;
+    r)
+      file=run.sh
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    # :)
+    #   echo "Option -$OPTARG requires an argument." >&2
+    #   exit 1
+    #   ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+language="${1^}"
+project="${2^}"
+
+if [[ $language == "" ]]; then
+    echo "Language not specified."
+elif [[ $project == "" ]]; then
+    echo "Project not specified."
+else
+    $scriptDir/$language/$project/$file
+fi
