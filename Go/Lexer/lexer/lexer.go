5a168bc541a81357ffc8b02d1e1dffa8e3af8032
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-22 22:09:53 -0700

    updated Go Lexer

--- Go/Lexer/lexer/lexer.go
+++ Go/Lexer/lexer/lexer.go
@@ -24,35 +24,79 @@ func (l *Lexer) NextToken() token.Token {
 	}
 	var tok token.Token
 
+	if l.Peek < len(l.Input) {
+		if ds := string(l.Input[l.Curr]) + string(l.Input[l.Peek]); ds == "//" {
+			return token.New(token.COMMENT, l.readComment())
+		} else if tt, ok := token.DoubleTokenMap[ds]; ok {
+			l.readChar()
+			l.readChar()
+			return token.New(tt, ds)
+		}
+	}
+
 	switch l.Char {
+	case 0:
+		return token.New(token.EOF, "0")
+	case '!':
+		tok = token.New(token.EXCLAMATION, "!")
+	case '@':
+		tok = token.New(token.AT, "@")
+	case '#':
+		tok = token.New(token.HASHTAG, "#")
+	case '$':
+		tok = token.New(token.DOLLAR, "$")
+	case '%':
+		tok = token.New(token.PERCENT, "%")
+	case '^':
+		tok = token.New(token.CARET, "^")
+	case '&':
+		tok = token.New(token.AMPERSAND, "&")
 	case '*':
 		tok = token.New(token.ASTERISK, "*")
+	case '(':
+		tok = token.New(token.LPAREN, "(")
+	case ')':
+		tok = token.New(token.RPAREN, ")")
 	case '-':
-		tok = token.New(token.DASH, "-")
+		tok = token.New(token.MINUS, "-")
+	case '_':
+		tok = token.New(token.UNDERSCORE, "_")
 	case '+':
 		tok = token.New(token.PLUS, "+")
 	case '=':
-		tok = token.New(token.EQUAL, "=")
+		tok = token.New(token.ASSIGN, "=")
+	case '[':
+		tok = token.New(token.LBRACKET, "[")
+	case ']':
+		tok = token.New(token.RBRACKET, "]")
+	case '{':
+		tok = token.New(token.LBRACE, "{")
+	case '}':
+		tok = token.New(token.RBRACE, "}")
+	case ';':
+		tok = token.New(token.SEMICOLON, ";")
+	case ':':
+		tok = token.New(token.COLON, ":")
+	case '\'':
+		tok = token.New(token.CHAR, l.readCharLiteral())
+	case '"':
+		tok = token.New(token.STRING, l.readString())
+	case ',':
+		tok = token.New(token.COMMA, ",")
+	case '.':
+		tok = token.New(token.PERIOD, ".")
 	case '<':
 		tok = token.New(token.LESSTHAN, "<")
 	case '>':
 		tok = token.New(token.GREATERTHAN, ">")
 	case '/':
 		tok = token.New(token.SLASH, "/")
-	case ';':
-		tok = token.New(token.SEMICOLON, ";")
-	case ':':
-		tok = token.New(token.COLON, ":")
-	case '(':
-		tok = token.New(token.LPAREN, "(")
-	case ')':
-		tok = token.New(token.RPAREN, ")")
-	case '{':
-		tok = token.New(token.LBRACE, "{")
-	case '}':
-		tok = token.New(token.RBRACE, "}")
-	case 0:
-		tok = token.New(token.EOF, "")
+	case '?':
+		tok = token.New(token.QUESTION, "?")
+	case '\\':
+		tok = token.New(token.BACKSLASH, "\\")
+	case '|':
+		tok = token.New(token.PIPE, "|")
 	default:
 		if isLetter(l.Char) {
 			ident := l.readIdentifier()
@@ -63,7 +107,8 @@ func (l *Lexer) NextToken() token.Token {
 			}
 			return tok
 		} else if isNumber(l.Char) {
-			return token.New(token.NUMBER, l.readNumber())
+			ttype, lit := l.readNumber()
+			return token.New(ttype, lit)
 		} else {
 			tok = token.New(token.ILLEGAL, "ILLEGAL")
 		}
@@ -81,9 +126,46 @@ func (l *Lexer) readIdentifier() string {
 	return l.Input[pos:l.Curr]
 }
 
-func (l *Lexer) readNumber() string {
+func (l *Lexer) readNumber() (token.TokenType, string) {
 	pos := l.Curr
-	for isNumber(l.Char) {
+	var is_float = false
+	for isNumber(l.Char) || l.Char == '.' {
+		if l.Char == '.' {
+			if is_float {
+				l.readChar()
+				return token.ILLEGAL, "ILLEGAL"
+			}
+		}
+		l.readChar()
+	}
+	if is_float {
+		return token.FLOAT, l.Input[pos:l.Curr]
+	} else {
+		return token.NUMBER, l.Input[pos:l.Curr]
+	}
+}
+
+func (l *Lexer) readString() string {
+	l.readChar()
+	pos := l.Curr
+	for l.Char != '"' {
+		l.readChar()
+	}
+	return l.Input[pos:l.Curr]
+}
+
+func (l *Lexer) readCharLiteral() string {
+	l.readChar()
+	pos := l.Curr
+	for l.Char != '\'' {
+		l.readChar()
+	}
+	return l.Input[pos:l.Curr]
+}
+
+func (l *Lexer) readComment() string {
+	pos := l.Curr + 2
+	for l.Char != '\n' && l.Char != '\r' {
 		l.readChar()
 	}
 	return l.Input[pos:l.Curr]
--- Go/Lexer/token/token.go
+++ Go/Lexer/token/token.go
@@ -6,32 +6,83 @@ const (
 	EOF = iota
 	ILLEGAL
 
+	// Core
 	LET
+	CONST
+	STRUCT
 	FUNCTION
 	IF
 	ELSE
+	SWITCH
+	CASE
+	BREAK
 	RETURN
+	WHILE
+	FOR
+	AND
+	OR
+	IN
 
+	// Types
 	IDENTIFIER
 	NUMBER
+	FLOAT
 	BOOLEAN
+	STRING
+	CHAR
 
+	// Datatypes
 	INT
+	F32
 	BOOL
+	STR
+	NIL
+	VOID
 
+	// Single Characters
+	EXCLAMATION // !
+	AT          // @
+	HASHTAG     // #
+	DOLLAR      // $
+	PERCENT     // %
+	CARET       // ^
+	AMPERSAND   // &
 	ASTERISK    // *
-	SLASH       // /
-	DASH        // -
-	PLUS        // +
-	EQUAL       // =
-	LESSTHAN    // <
-	GREATERTHAN // >
-	SEMICOLON   // ;
-	COLON       // :
 	LPAREN      // (
 	RPAREN      // )
+	MINUS       // -
+	UNDERSCORE  // _
+	PLUS        // +
+	ASSIGN      // =
+	LBRACKET    // [
+	RBRACKET    // ]
 	LBRACE      // {
 	RBRACE      // }
+	SEMICOLON   // ;
+	COLON       // :
+	APOSTROPHE  // '
+	QUOTE       // "
+	COMMA       // ,
+	PERIOD      // .
+	LESSTHAN    // <
+	GREATERTHAN // >
+	SLASH       // /
+	QUESTION    // ?
+	BACKSLASH   // \/
+	PIPE        // |
+
+	// Double Characters
+	EQUAL     // ==
+	NOT_EQUAL // !=
+	PLUS_EQ   // +=
+	MINUS_EQ  // -=
+	MULT_EQ   // *=
+	DIV_EQ    // /=
+	LT_EQUAL  // <=
+	GT_EQUAL  // >=
+	INCREMENT // ++
+	DECREMENT // --
+	COMMENT   // //
 )
 
 type Token struct {
@@ -48,14 +99,199 @@ func New(ttype TokenType, lit string) Token {
 
 var IdentifierMap = map[string]TokenType{
 	"let":    LET,
+	"const":  CONST,
+	"struct": STRUCT,
 	"fn":     FUNCTION,
 	"if":     IF,
 	"else":   ELSE,
+	"switch": SWITCH,
+	"case":   CASE,
+	"break":  BREAK,
 	"return": RETURN,
+	"while":  WHILE,
+	"for":    FOR,
+	"and":    AND,
+	"or":     OR,
+	"in":     IN,
 
 	"true":  BOOLEAN,
 	"false": BOOLEAN,
 
 	"bool": BOOL,
 	"int":  INT,
+	"f32":  F32,
+	"str":  STR,
+	"nil":  NIL,
+	"void": VOID,
+}
+
+var DoubleTokenMap = map[string]TokenType{
+	"==": EQUAL,
+	"!=": NOT_EQUAL,
+	"+=": PLUS_EQ,
+	"-=": MINUS_EQ,
+	"*=": MULT_EQ,
+	"/=": DIV_EQ,
+	"<=": LT_EQUAL,
+	">=": GT_EQUAL,
+	"++": INCREMENT,
+	"--": DECREMENT,
+	"//": COMMENT,
+}
+
+func (t TokenType) String() string {
+	switch t {
+	case EOF:
+		return "EOF"
+	case ILLEGAL:
+		return "ILLEGAL"
+
+	// Core
+	case LET:
+		return "LET"
+	case CONST:
+		return "CONST"
+	case STRUCT:
+		return "STRUCT"
+	case FUNCTION:
+		return "FUNCTION"
+	case IF:
+		return "IF"
+	case ELSE:
+		return "ELSE"
+	case SWITCH:
+		return "SWITCH"
+	case CASE:
+		return "CASE"
+	case BREAK:
+		return "BREAK"
+	case RETURN:
+		return "RETURN"
+	case WHILE:
+		return "WHILE"
+	case FOR:
+		return "FOR"
+	case AND:
+		return "AND"
+	case OR:
+		return "OR"
+	case IN:
+		return "IN"
+
+	// Types
+	case IDENTIFIER:
+		return "IDENTIFIER"
+	case NUMBER:
+		return "NUMBER"
+	case FLOAT:
+		return "FLOAT"
+	case BOOLEAN:
+		return "BOOLEAN"
+	case STRING:
+		return "STRING"
+	case CHAR:
+		return "CHAR"
+
+	// Datatypes
+	case INT:
+		return "INT"
+	case F32:
+		return "F32"
+	case BOOL:
+		return "BOOL"
+	case STR:
+		return "STR"
+	case NIL:
+		return "NIL"
+	case VOID:
+		return "VOID"
+
+	// Single Characters
+	case EXCLAMATION:
+		return "EXCLAMATION"
+	case AT:
+		return "AT"
+	case HASHTAG:
+		return "HASHTAG"
+	case DOLLAR:
+		return "DOLLAR"
+	case PERCENT:
+		return "PERCENT"
+	case CARET:
+		return "CARET"
+	case AMPERSAND:
+		return "AMPERSAND"
+	case ASTERISK:
+		return "ASTERISK"
+	case LPAREN:
+		return "LPAREN"
+	case RPAREN:
+		return "RPAREN"
+	case MINUS:
+		return "MINUS"
+	case UNDERSCORE:
+		return "UNDERSCORE"
+	case PLUS:
+		return "PLUS"
+	case ASSIGN:
+		return "ASSIGN"
+	case LBRACKET:
+		return "LBRACKET"
+	case RBRACKET:
+		return "RBRACKET"
+	case LBRACE:
+		return "LBRACE"
+	case RBRACE:
+		return "RBRACE"
+	case SEMICOLON:
+		return "SEMICOLON"
+	case COLON:
+		return "COLON"
+	case APOSTROPHE:
+		return "APOSTROPHE"
+	case QUOTE:
+		return "QUOTE"
+	case COMMA:
+		return "COMMA"
+	case PERIOD:
+		return "PERIOD"
+	case LESSTHAN:
+		return "LESSTHAN"
+	case GREATERTHAN:
+		return "GREATERTHAN"
+	case SLASH:
+		return "SLASH"
+	case QUESTION:
+		return "QUESTION"
+	case BACKSLASH:
+		return "BACKSLASH"
+	case PIPE:
+		return "PIPE"
+
+	// Double Characters
+	case EQUAL:
+		return "EQUAL"
+	case NOT_EQUAL:
+		return "NOT_EQUAL"
+	case PLUS_EQ:
+		return "PLUS_EQ"
+	case MINUS_EQ:
+		return "MINUS_EQ"
+	case MULT_EQ:
+		return "MULT_EQ"
+	case DIV_EQ:
+		return "DIV_EQ"
+	case LT_EQUAL:
+		return "LT_EQUAL"
+	case GT_EQUAL:
+		return "GT_EQUAL"
+	case INCREMENT:
+		return "INCREMENT"
+	case DECREMENT:
+		return "DECREMENT"
+	case COMMENT:
+		return "COMMENT"
+	default:
+		return ""
+	}
 }
