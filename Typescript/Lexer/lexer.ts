8d23042aaf5e1cc5bd4b9040168b1c16c02a00a0
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-25 13:36:29 -0700

    feat(ts): updated lexer

--- Typescript/Lexer/lexer.ts
+++ Typescript/Lexer/lexer.ts
@@ -1,4 +1,4 @@
-import { Token, TokenType, newToken, IDENTIFIER_MAP } from "./token";
+import { Token, TokenType, newToken, IDENTIFIER_MAP, DOUBLE_TOKEN_MAP } from "./token";
 
 export class Lexer {
   private input: string;
@@ -20,21 +20,50 @@ export class Lexer {
     while (isWhitespace(this.ch))
       this.readChar();
 
+    if (this.peek < this.input.length) {
+      const ds: string = this.input[this.curr] + this.input[this.peek]
+      if (ds == "//")
+        return newToken(TokenType.COMMENT, this.readComment());
+      const ttype = DOUBLE_TOKEN_MAP[ds];
+      if (ttype != null && ttype != undefined) {
+        this.readChar();
+        this.readChar();
+        return newToken(ttype, ds);
+      }
+    }
+
     switch (this.ch) {
       case '\0': return tok;
+      case '!': tok = newToken(TokenType.EXCLAMATION, "!"); break;
+      case '@': tok = newToken(TokenType.AT, "@"); break;
+      case '#': tok = newToken(TokenType.HASHTAG, "#"); break;
+      case '$': tok = newToken(TokenType.DOLLAR, "$"); break;
+      case '%': tok = newToken(TokenType.PERCENT, "%"); break;
+      case '^': tok = newToken(TokenType.CARET, "^"); break;
+      case '&': tok = newToken(TokenType.AMPERSAND, "&"); break;
       case '*': tok = newToken(TokenType.ASTERISK, "*"); break;
-      case '-': tok = newToken(TokenType.DASH, "-"); break;
+      case '(': tok = newToken(TokenType.LPAREN, "("); break;
+      case ')': tok = newToken(TokenType.RPAREN, ")"); break;
+      case '-': tok = newToken(TokenType.MINUS, "-"); break;
+      case '_': tok = newToken(TokenType.UNDERSCORE, "_"); break;
       case '+': tok = newToken(TokenType.PLUS, "+"); break;
-      case '=': tok = newToken(TokenType.EQUAL, "="); break;
+      case '=': tok = newToken(TokenType.ASSIGN, "="); break;
+      case '[': tok = newToken(TokenType.LBRACKET, "["); break;
+      case ']': tok = newToken(TokenType.RBRACKET, "]"); break;
+      case '{': tok = newToken(TokenType.LBRACE, "{"); break;
+      case '}': tok = newToken(TokenType.RBRACE, "}"); break;
+      case ';': tok = newToken(TokenType.SEMICOLON, ";"); break;
+      case ':': tok = newToken(TokenType.COLON, ":"); break;
+      case '\'': tok = newToken(TokenType.CHAR, this.readCharLiteral()); break;
+      case '"': tok = newToken(TokenType.STRING, this.readString()); break;
+      case ',': tok = newToken(TokenType.COMMA, ","); break;
+      case '.': tok = newToken(TokenType.PERIOD, "."); break;
       case '<': tok = newToken(TokenType.LESSTHAN, "<"); break;
       case '>': tok = newToken(TokenType.GREATERTHAN, ">"); break;
       case '/': tok = newToken(TokenType.SLASH, "/"); break;
-      case ';': tok = newToken(TokenType.SEMICOLON, ";"); break;
-      case ':': tok = newToken(TokenType.COLON, ";"); break;
-      case '(': tok = newToken(TokenType.LPAREN, "("); break;
-      case ')': tok = newToken(TokenType.RPAREN, ")"); break;
-      case '{': tok = newToken(TokenType.LBRACE, "{"); break;
-      case '}': tok = newToken(TokenType.RBRACE, "}"); break;
+      case '?': tok = newToken(TokenType.QUESTION, "?"); break;
+      case '\\': tok = newToken(TokenType.BACKSLASH, "\\"); break;
+      case '|': tok = newToken(TokenType.PIPE, "|"); break;
       default:
         if (isLetter(this.ch)) {
           const str = this.readIdentifier();
@@ -42,9 +71,11 @@ export class Lexer {
           return ttype == null || ttype == undefined ?
             newToken(TokenType.IDENTIFIER, str) :
             newToken(ttype, str);
-        } else if (isDigit(this.ch))
-          return newToken(TokenType.NUMBER, this.readNumber());
-        else return newToken(TokenType.ILLEGAL, "ILLEGAL");
+        } else if (isDigit(this.ch)) {
+          const [ttype, lit] = this.readNumber();
+          return newToken(ttype, lit);
+        }
+        return newToken(TokenType.ILLEGAL, "ILLEGAL");
     }
     this.readChar();
     return tok;
@@ -52,16 +83,50 @@ export class Lexer {
 
   private readIdentifier(): string {
     const pos = this.curr;
-    while (isLetter(this.ch) || this.ch == '_')
+    while (isLetter(this.ch) || isDigit(this.ch) || this.ch == '_')
       this.readChar();
     return this.input.substring(pos, this.curr);
   }
 
-  private readNumber(): string {
+  private readString(): string {
+    this.readChar();
     const pos = this.curr;
-    while (isDigit(this.ch))
+    while (this.ch != '"' && this.ch != '\0')
       this.readChar();
     return this.input.substring(pos, this.curr);
+  }
+
+  private readCharLiteral(): string {
+    this.readChar();
+    const pos = this.curr;
+    while (this.ch != "'" && this.ch != '\0')
+      this.readChar();
+    return this.input.substring(pos, this.curr);
+  }
+
+  private readComment(): string {
+    const pos = this.curr;
+    while (this.ch != '\n' && this.ch != '\r' && this.ch != '\0')
+      this.readChar();
+    return this.input.substring(pos, this.curr);
+  }
+
+  private readNumber(): [TokenType, string] {
+    const pos = this.curr;
+    let is_float = false;
+    while (isDigit(this.ch) || this.ch == '.') {
+      if (this.ch == '.') {
+        if (is_float) {
+          this.readChar();
+          return [TokenType.ILLEGAL, "ILLEGAL"];
+        }
+        is_float = true;
+      }
+      this.readChar();
+    }
+    if (is_float)
+      return [TokenType.FLOAT, this.input.substring(pos, this.curr)];
+    return [TokenType.NUMBER, this.input.substring(pos, this.curr)];
   }
 
   private readChar() {
@@ -74,7 +139,7 @@ export class Lexer {
 }
 
 function isLetter(str: string): boolean {
-  return /^[a-xA-Z]+$/.test(str);
+  return /^[a-zA-Z]+$/.test(str);
 }
 
 function isDigit(str: string): boolean {
--- Typescript/Lexer/token.ts
+++ Typescript/Lexer/token.ts
@@ -1,33 +1,84 @@
 export enum TokenType {
-  EOF = "EOF",
-  ILLEGAL = "ILLEGAL",
+    EOF = "EOF",
+    ILLEGAL = "ILLEGAL",
 
-  LET = "LET",
-  FUNCTION = "FUNCTION",
-  IF = "IF",
-  ELSE = "ELSE",
-  RETURN = "RETURN",
+    // Core
+    LET = "LET",
+    CONST = "CONST",
+    STRUCT = "STRUCT",
+    FUNCTION = "FUNCTION",
+    IF = "IF",
+    ELSE = "ELSE",
+    SWITCH = "SWITCH",
+    CASE = "CASE",
+    BREAK = "BREAK",
+    RETURN = "RETURN",
+    WHILE = "WHILE",
+    FOR = "FOR",
+    AND = "AND",
+    OR = "OR",
+    IN = "IN",
 
-  IDENTIFIER = "IDENTIFIER",
-  NUMBER = "NUMBER",
-  BOOLEAN = "BOOLEAN",
+    // Types
+    IDENTIFIER = "IDENTIFIER",
+    NUMBER = "NUMBER",
+    FLOAT = "FLOAT",
+    BOOLEAN = "BOOLEAN",
+    STRING = "STRING",
+    CHAR = "CHAR",
 
-  INT = "INT",
-  BOOL = "BOOL",
+    // Datatypes
+    INT = "INT",
+    F32 = "F32",
+    BOOL = "BOOL",
+    STR = "STR",
+    NIL = "NIL",
+    VOID = "VOID",
 
-  ASTERISK = "ASTERISK",
-  SLASH = "SLASH",
-  DASH = "DASH",
-  PLUS = "PLUS",
-  EQUAL = "EQUAL",
-  LESSTHAN = "LESSTHAN",
-  GREATERTHAN = "GREATERTHAN",
-  SEMICOLON = "SEMICOLON",
-  COLON = "COLON",
-  LPAREN = "LPAREN",
-  RPAREN = "RPAREN",
-  LBRACE = "LBRACE",
-  RBRACE = "RBRACE",
+    // Single Characters
+    EXCLAMATION = "EXCLAMATION", // !
+    AT = "AT",          // @
+    HASHTAG = "HASHTAG",     // #
+    DOLLAR = "DOLLAR",      // $
+    PERCENT = "PERCENT",     // %
+    CARET = "CARET",       // ^
+    AMPERSAND = "AMPERSAND",   // &
+    ASTERISK = "ASTERISK",    // *
+    LPAREN = "LPAREN",      // (
+    RPAREN = "RPAREN",      // )
+    MINUS = "MINUS",       // -
+    UNDERSCORE = "UNDERSCORE",  // _
+    PLUS = "PLUS",        // +
+    ASSIGN = "ASSIGN",      // =
+    LBRACKET = "LBRACKET",    // [
+    RBRACKET = "RBRACKET",    // ]
+    LBRACE = "LBRACE",      // {
+    RBRACE = "RBRACE",      // }
+    SEMICOLON = "SEMICOLON",   // ;
+    COLON = "COLON",       // :
+    APOSTROPHE = "APOSTROPHE",  // '
+    QUOTE = "QUOTE",       // "
+    COMMA = "COMMA",       // ,
+    PERIOD = "PERIOD",      // .
+    LESSTHAN = "LESSTHAN",    // <
+    GREATERTHAN = "GREATERTHAN", // >
+    SLASH = "SLASH",       // /
+    QUESTION = "QUESTION",    // ?
+    BACKSLASH = "BACKSLASH",   // \/
+    PIPE = "PIPE",        // |
+
+    // Double Characters
+    EQUAL = "EQUAL",     // ==
+    NOTEQUAL = "NOT_EQUAL", // !=
+    PLUSEQ = "PLUS_EQ",   // +=
+    MINUSEQ = "MINUS_EQ",  // -=
+    MULTEQ = "MULT_EQ",   // *=
+    DIVEQ = "DIV_EQ",    // /=
+    LTEQUAL = "LT_EQUAL",  // <=
+    GTEQUAL = "GT_EQUAL",  // >=
+    INCREMENT = "INCREMENT", // ++
+    DECREMENT = "DECREMENT", // --
+    COMMENT = "COMMENT",   // //
 }
 
 export type Token = {
@@ -40,15 +91,43 @@ export function newToken(type: TokenType, literal: string): Token {
 }
 
 export const IDENTIFIER_MAP = {
-  "let": TokenType.LET,
-  "fn": TokenType.FUNCTION,
-  "if": TokenType.IF,
-  "else": TokenType.ELSE,
-  "return": TokenType.RETURN,
+    "let": TokenType.LET,
+    "const": TokenType.CONST,
+    "struct": TokenType.STRUCT,
+    "fn": TokenType.FUNCTION,
+    "if": TokenType.IF,
+    "else": TokenType.ELSE,
+    "switch": TokenType.SWITCH,
+    "case": TokenType.CASE,
+    "break": TokenType.BREAK,
+    "return": TokenType.RETURN,
+    "while": TokenType.WHILE,
+    "for": TokenType.FOR,
+    "and": TokenType.AND,
+    "or": TokenType.OR,
+    "in": TokenType.IN,
 
-  "true": TokenType.BOOLEAN,
-  "false": TokenType.BOOLEAN,
+    "true": TokenType.BOOLEAN,
+    "false": TokenType.BOOLEAN,
 
-  "bool": TokenType.BOOL,
-  "int": TokenType.INT,
+    "bool": TokenType.BOOL,
+    "int": TokenType.INT,
+    "f32": TokenType.F32,
+    "str": TokenType.STR,
+    "nil": TokenType.NIL,
+    "void": TokenType.VOID,
+}
+
+export const DOUBLE_TOKEN_MAP = {
+    "==": TokenType.EQUAL,
+    "!=": TokenType.NOTEQUAL,
+    "+=": TokenType.PLUSEQ,
+    "-=": TokenType.MINUSEQ,
+    "*=": TokenType.MULTEQ,
+    "/=": TokenType.DIVEQ,
+    "<=": TokenType.LTEQUAL,
+    ">=": TokenType.GTEQUAL,
+    "++": TokenType.INCREMENT,
+    "--": TokenType.DECREMENT,
+    "//": TokenType.COMMENT,
 }
