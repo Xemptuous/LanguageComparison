3f460637b0679b9ba7c64b4164ecd1b03b1b21b2
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2025-04-25 10:43:39 -0700

    feat(nim): updated lexer

--- Nim/Lexer/main.nim
+++ Nim/Lexer/main.nim
@@ -4,15 +4,45 @@ import lexer
 
 proc main() = 
     let input = """
-	fn is_gt_ten(num: int) bool {
-		if (num > 10) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	let ten = 5 + 5 * 4 / 2 - 5;
-	print(is_big(ten));
+        struct Node {
+            data: int;
+            left: *Node;
+            right: *Node;
+        }
+        fn add_nums(x: int, y: int) int {
+            return x + y;
+        }
+
+        // check if str starts with an h
+        fn starts_with_h(s: str) bool {
+            return s[0] == 'H';
+        }
+
+        fn printNum(n: int|f32) void {
+            print(n);
+        }
+        let rootNode = Node{5, nil, nil};
+        print(rootNode.data);
+
+        const ten: int = 5 + 5 * 4 / 2 - 5;
+        const newNum = add_nums(ten, 25);
+        const isTen = ten == 10 ? true : false;
+        const nothing = nil;
+
+        let mystring: str = "Hello, Lexer!";
+        for ch: str in mystring {
+            print(ch); // a comment here!
+        }
+
+        const myfloat: f32 = 69.420;
+
+        let counter = 0; 
+        let otherCounter = 20; 
+
+        while counter < 10 or otherCounter > 10 {
+            otherCounter -= 1;
+            counter++;
+        }
     """
     var lex: Lexer = newLexer(input)
 
--- Nim/Lexer/token.nim
+++ Nim/Lexer/token.nim
@@ -4,32 +4,85 @@ type TokenType* = enum
     Eof = "EOF",
     Illegal = "ILLEGAL",
 
+    # Core
     Let = "LET",
+    Const = "CONST",
+    Struct = "STRUCT",
     Function = "FUNCTION",
     If = "IF",
     Else = "ELSE",
+    Switch = "SWITCH",
+    Case = "CASE",
+    Break = "BREAK",
     Return = "RETURN",
+    While = "WHILE",
+    For = "FOR",
+    And = "AND",
+    Or = "OR",
+    In = "IN",
 
+    # Types
     Identifier = "IDENTIFIER",
     Number = "NUMBER",
+    Float = "FLOAT",
     Boolean = "BOOLEAN",
+    String = "STRING",
+    Char = "CHAR",
 
+    # Datatypes
     Int = "INT",
+    F32 = "F32",
     Bool = "BOOL",
+    Str = "STR",
+    Nil = "NIL",
+    Void = "VOID",
 
-    Asterisk = "ASTERISK",
-    Slash = "SLASH",
-    Dash = "DASH",
-    Plus = "PLUS",
-    Equal = "EQUAL",
-    Lessthan = "LESSTHAN",
-    Greaterthan = "GREATERTHAN",
-    Semicolon = "SEMICOLON",
-    Colon = "COLON",
-    Lparen = "LPAREN",
-    Rparen = "RPAREN",
-    Lbrace = "LBRACE",
-    Rbrace = "RBRACE",
+    # Single Characters
+    Exclamation = "EXCLAMATION", # !
+    At = "AT",          # @
+    Hashtag = "HASHTAG",     # #
+    Dollar = "DOLLAR",      # $
+    Percent = "PERCENT",     # %
+    Caret = "CARET",       # ^
+    Ampersand = "AMPERSAND",   # &
+    Asterisk = "ASTERISK",    # *
+    Lparen = "LPAREN",      # (
+    Rparen = "RPAREN",      # )
+    Minus = "MINUS",       # -
+    Underscore = "UNDERSCORE",  # _
+    Plus = "PLUS",        # +
+    Assign = "ASSIGN",      # =
+    Lbracket = "LBRACKET",    # [
+    Rbracket = "RBRACKET",    # ]
+    Lbrace = "LBRACE",      # {
+    Rbrace = "RBRACE",      # }
+    Semicolon = "SEMICOLON",   # ;
+    Colon = "COLON",       # :
+    Apostrophe = "APOSTROPHE",  # '
+    Quote = "QUOTE",       # "
+    Comma = "COMMA",       # ,
+    Period = "PERIOD",      # .
+    Lessthan = "LESSTHAN",    # <
+    Greaterthan = "GREATERTHAN", # >
+    Slash = "SLASH",       # /
+    Question = "QUESTION",    # ?
+    Backslash = "BACKSLASH",   # \/
+    Pipe = "PIPE",        # |
+
+    # Double Characters
+    Equal = "EQUAL",     # ==
+    NotEqual = "NOT_EQUAL", # !=
+    PlusEq = "PLUS_EQ",   # +=
+    MinusEq = "MINUS_EQ",  # -=
+    MultEq = "MULT_EQ",   # *=
+    DivEq = "DIV_EQ",    # /=
+    LtEqual = "LT_EQUAL",  # <=
+    GtEqual = "GT_EQUAL",  # >=
+    Increment = "INCREMENT", # ++
+    Decrement = "DECREMENT", # --
+    Comment = "COMMENT",   # //
+
+
 
 type Token* = object
     token_type*: TokenType
@@ -38,30 +91,44 @@ type Token* = object
 proc newToken*(token_type: TokenType, literal: string): Token =
     return Token(token_type: token_type, literal: literal)
 
-proc getIdentifierMap*(): Table[string, TokenType] =
-    var map = initTable[string, TokenType]()
-    map["let"] = TokenType.Let
-    map["fn"] = TokenType.Function
-    map["if"] = TokenType.If
-    map["else"] = TokenType.Else
-    map["return"] = TokenType.Return
-
-    map["true"] = TokenType.Boolean
-    map["false"] = TokenType.Boolean
-
-    map["bool"] = TokenType.Bool
-    map["int"] = TokenType.Int
-    return map
-
-
 const IDENTIFIER_MAP* = {
     "let": TokenType.Let,
+    "const": TokenType.Const,
+    "struct": TokenType.Struct,
     "fn": TokenType.Function,
     "if": TokenType.If,
     "else": TokenType.Else,
+    "switch": TokenType.Switch,
+    "case": TokenType.Case,
+    "break": TokenType.Break,
     "return": TokenType.Return,
+    "while": TokenType.While,
+    "for": TokenType.For,
+    "and": TokenType.And,
+    "or": TokenType.Or,
+    "in": TokenType.In,
+
     "true": TokenType.Boolean,
     "false": TokenType.Boolean,
+
     "bool": TokenType.Bool,
-    "int": TokenType.Int
+    "int": TokenType.Int,
+    "f32": TokenType.F32,
+    "str": TokenType.Str,
+    "nil": TokenType.Nil,
+    "void": TokenType.Void,
+}.toTable()
+
+const DOUBLE_TOKEN_MAP* = {
+    "==": TokenType.EQUAL,
+    "!=": TokenType.NOT_EQUAL,
+    "+=": TokenType.PLUS_EQ,
+    "-=": TokenType.MINUS_EQ,
+    "*=": TokenType.MULT_EQ,
+    "/=": TokenType.DIV_EQ,
+    "<=": TokenType.LT_EQUAL,
+    ">=": TokenType.GT_EQUAL,
+    "++": TokenType.INCREMENT,
+    "--": TokenType.DECREMENT,
+    "//": TokenType.COMMENT,
 }.toTable()
